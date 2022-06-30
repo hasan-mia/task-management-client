162266974959a9f7d8cb58c8a9b600483a352dd4
hasanrafi69 <hasanrafi69@gmail.com>
2022-06-30 23:08:11 +0600

    date added

--- src/Components/Calender/Calendar.js
+++ src/Components/Calender/Calendar.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import { getMonth, getYear } from 'date-fns'
+import range from 'lodash/range'
+import 'react-datepicker/dist/react-datepicker.css'
 
 const Calendar = () => {
+  	const [startDate, setStartDate] = useState(new Date());
+  	const years = range(1990, getYear(new Date()) + 1, 1);
+  	const months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
 	return (
 		<main className='w-full px-14 py-8'>
-			Calendar 
+			<DatePicker renderCustomHeader={({date, changeYear, changeMonth, decreaseMonth, increaseMonth, prevMonthButtonDisabled, nextMonthButtonDisabled, }) => (
+			<div className='flex flex-wrap justify-center items-center m-4 text-lg'>
+				<button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+					{"<"}
+				</button>
+				<select value={getYear(date)}onChange={({ target: { value } }) => changeYear(value)}>
+					{years.map((option) => (
+					<option key={option} value={option}>
+						{option}
+					</option>
+					))}
+				</select>
+
+				<select
+					value={months[getMonth(date)]}
+					onChange={({ target: { value } }) =>
+					changeMonth(months.indexOf(value))
+					}
+				>
+					{months.map((option) => (
+					<option key={option} value={option}>
+						{option}
+					</option>
+					))}
+				</select>
+
+				<button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+					{">"}
+				</button>
+			</div>
+			)}
+			selected={startDate}
+			onChange={(date) => setStartDate(date)}
+			/>
 		</main>
 	);
 };
