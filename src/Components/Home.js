b50c51d896c0582dccf31a8137b237c6502cb3b4
hasanrafi69 <hasanrafi69@gmail.com>
2022-07-01 11:51:26 +0600

    ok done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 		<main className='w-full p-8'>
 			<div className='flex flex-wrap flex-row justify-center'>
 
-				<div className='flex-shrink max-w-full px-0 my-2 w-12/12 sm:w-12/12 md:w-6/12 lg:w-3/12 xl:px-6 border lg:border-0 left-siebar'>
+				<div className='flex-shrink w-full px-0 my-2 w-12/12 sm:w-12/12 md:w-6/12 lg:w-3/12 xl:px-6 border lg:border-0 left-siebar'>
 					<div className="text-xl mb-3 font-semibold">
 						<h2 className='uppercase pb-2 text-center lg:text-left'>ADD YOUR TASK</h2>
 					</div>
@@ -17,7 +17,7 @@ const Home = () => {
 					</div>
 				</div>
 
-				<div className='flex-shrink max-w-full px-0 my-2 w-12/12 sm:w-12/12 md:w-6/12 lg:w-9/12 xl:px-6 border lg:border-0'>
+				<div className='flex-shrink w-full px-0 my-2 w-12/12 sm:w-12/12 md:w-6/12 lg:w-9/12 xl:px-6 border lg:border-0'>
 					<div className="text-xl mb-3 font-semibold">
 						<h2 className='uppercase mt-6 lg:mt-0 pb-2 text-center lg:text-left'>YOUR TASK LIST</h2>
 					</div>
